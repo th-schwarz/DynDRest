0008bf50e3b160c3985d2d2ad86615ae45b8c1f3
Thilo Schwarz <osp@thischwa.codes>
2023-05-07 19:34:24 +0200

    .

--- src/main/java/codes/thischwa/dyndrest/Starter.java
+++ src/main/java/codes/thischwa/dyndrest/Starter.java
@@ -10,7 +10,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 /**
- * The starter. of the application.
+ * The starter of the application.
  */
 @Slf4j
 @SpringBootApplication
