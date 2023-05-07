37e98d863303900ac50e8dbd64f42c8eb9646cec
Thilo Schwarz <osp@thischwa.codes>
2023-05-07 19:07:40 +0200

    .

--- src/test/java/codes/thischwa/dyndrest/model/UpdateItemTest.java
+++ src/test/java/codes/thischwa/dyndrest/model/UpdateItemTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class UpdateItemTest {
+class UpdateItemTest {
 
   @Test
   final void testItem() {
