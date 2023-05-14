cc7da08cafd7d5060942afccfaa2b3683c498fea
Thilo Schwarz <osp@thischwa.codes>
2023-05-14 18:26:50 +0200

    fixed docu

--- src/main/java/codes/thischwa/dyndrest/ApiRoutes.java
+++ src/main/java/codes/thischwa/dyndrest/ApiRoutes.java
@@ -25,13 +25,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 //@formatter:off
 @OpenAPIDefinition(
     info = @Info(
-        title = "A Dynamic DNS REST service.",
+        title = "A Dynamic DNS REST Service.",
         description = "The routes of the dynamic DNS API",
         version = "0.1",
         contact = @Contact(
             name = "Thilo Schwarz",
             email = "osp (at) thischwa.codes",
-            url = "https://thischwa.codes"
+            url = "https://github.com/th-schwarz/DynDRest"
         ),
         license = @License(
             name = "MIT Licence",
@@ -47,8 +47,7 @@ interface ApiRoutes {
 
   @Operation(summary = "Updates the desired IP addresses of the 'host', if the 'apitoken' belongs to the host. If both parameters for IP addresses aren't set, an attempt is made to fetch the remote IP.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Updates the desired IP addresses of the 'host', if the 'apitoken' belongs to the host. If both parameters for IP addresses aren't set, an attempt is made to fetch the remote IP.", content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, examples = {
-          @ExampleObject(value = "Update successful.")})),
+      @ApiResponse(responseCode = "200", description = "Updates the desired IP addresses of the 'host', if the 'apitoken' belongs to the host. If both parameters for IP addresses aren't set, an attempt is made to fetch the remote IP."),
       @ApiResponse(responseCode = "400", description = "If the 'apitoken' doesn't belong to the host, IP addresses aren't valid or the remote IP couldn't determine."),
       @ApiResponse(responseCode = "500", description = "If the update fails.")})
   @GetMapping(value = "/update/{host}", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -61,10 +60,11 @@ interface ApiRoutes {
 
   @Operation(summary = "Determines the IP settings of the 'host' and returns it in a JSON object.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "A JSON object with the IP settings of the 'host'"),
-      @ApiResponse(responseCode = "400", description = "If the 'apitoken' doesn't belong to the host, IP addresses aren't valid or the remote IP couldn't determine."),
-      @ApiResponse(responseCode = "404", description = "If the 'host' isn't configured."),
-      @ApiResponse(responseCode = "500", description = "If the zone info fails.")})
+      @ApiResponse(responseCode = "200", description = "A JSON object with the IP settings of the 'host'.",
+          content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, examples = @ExampleObject(value = "{\"ipv4\":\"127.1.2.4\",\"ipv6\":\"2a03:4000:41:32:0:0:0:2\"}"))),
+      @ApiResponse(responseCode = "400", description = "If the 'apitoken' doesn't belong to the host, IP addresses aren't valid or the remote IP couldn't determine.", content = @Content),
+      @ApiResponse(responseCode = "404", description = "If the 'host' isn't configured.", content = @Content),
+      @ApiResponse(responseCode = "500", description = "If the zone info fails.", content = @Content)})
   @GetMapping(value = "/info/{host}", produces = MediaType.APPLICATION_JSON_VALUE)
   ResponseEntity<IpSetting> info(
       @Parameter(description = "The host, for which the IPs must be determined.", content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, examples = {
@@ -72,7 +72,7 @@ interface ApiRoutes {
       @Parameter(description = "The 'apitoken' which belongs to the 'host'.", content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)) @RequestParam String apitoken);
 
   @Operation(summary = "Delivers pageable update logs.")
-  @ApiResponse(responseCode = "200", description = "Order list of update logs.")
+  @ApiResponse(responseCode = "200", description = "Ordered list of update logs.")
   @GetMapping(value = "info/update-log", produces = MediaType.APPLICATION_JSON_VALUE)
   ResponseEntity<UpdateLogPage> deliverLogs(
       @Parameter(description = "Page number to fetch. If it is not committed, it will be '1'.", content = @Content(mediaType = "int")) @Nullable @RequestParam(required = false) Integer page,
