489531066d861ae029f93a0304066372c2b49216
Thilo Schwarz <osp@thischwa.codes>
2023-07-10 11:08:28 +0200

    cleaned up

--- src/main/java/codes/thischwa/dyndrest/config/SecurityConfig.java
+++ src/main/java/codes/thischwa/dyndrest/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -65,7 +64,7 @@ public class SecurityConfig {
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    HttpBasicConfigurer<HttpSecurity> configurer = http
+    http
 
         // public routes
         .authorizeHttpRequests().requestMatchers("/", "/favicon.ico", "/v3/api-docs*")
@@ -80,7 +79,7 @@ public class SecurityConfig {
         .and()
         .httpBasic();
 
-    return configurer.and().build();
+    return http.build();
   }
 
 }
