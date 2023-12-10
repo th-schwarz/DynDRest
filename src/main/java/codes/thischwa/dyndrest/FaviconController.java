8d57b1ad8bd2cef35bd7342f32ab7b40f3aab4df
Thilo Schwarz <osp@thischwa.codes>
2023-12-10 19:40:57 +0100

    small refactoring

--- src/main/java/codes/thischwa/dyndrest/FaviconController.java
+++ src/main/java/codes/thischwa/dyndrest/FaviconController.java
@@ -1,19 +1,17 @@
 package codes.thischwa.dyndrest;
 
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for favicon.ico, just for reducing 404 errors in the logs.
  */
-@Controller
+@RestController
 public class FaviconController {
 
   @Operation(hidden = true)
   @GetMapping("favicon.ico")
-  @ResponseBody
   void returnNoFavicon() {
     // see class comment
   }
