05116523186078a6b99eb31e0816533843bd0b60
Thilo Schwarz <osp@thischwa.codes>
2024-02-26 18:42:30 +0100

    Update log file pattern in settings and test

--- src/test/java/codes/thischwa/dyndrest/config/AppConfigTest.java
+++ src/test/java/codes/thischwa/dyndrest/config/AppConfigTest.java
@@ -20,7 +20,7 @@ class AppConfigTest extends AbstractIntegrationTest {
     assertEquals("domainrobot", appConfig.provider());
 
     assertEquals(
-        "classpath:/test-files/dyndrest-update*", appConfig.updateLogFilePattern());
+        "file:target/test-classes/test-files/dyndrest-update*", appConfig.updateLogFilePattern());
     assertEquals("(.*)\\s+-\\s+([a-zA-Z\\.-]*)\\s+(\\S*)\\s+(\\S*)", appConfig.updateLogPattern());
     assertEquals("%d{yyyy-MM-dd HH:mm:ss.SSS} - %msg%n", appConfig.updateLogEncoderPattern());
     assertEquals("yyyy-MM-dd HH:mm:SSS", appConfig.updateLogDatePattern());
