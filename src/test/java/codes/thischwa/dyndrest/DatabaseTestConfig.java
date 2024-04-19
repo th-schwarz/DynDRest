76bfc613b976fec85d53b0bff8095699f2debb29
Thilo Schwarz <osp@thischwa.codes>
2024-04-19 15:51:36 +0200

    Update database configuration and enhance test setup

--- src/test/java/codes/thischwa/dyndrest/DatabaseTestConfig.java
+++ src/test/java/codes/thischwa/dyndrest/DatabaseTestConfig.java
@@ -2,18 +2,20 @@ package codes.thischwa.dyndrest;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
-import javax.sql.DataSource;
-
+import codes.thischwa.dyndrest.model.UpdateLog;
 import codes.thischwa.dyndrest.model.converter.EnumToStringConverter;
 import codes.thischwa.dyndrest.model.converter.StringToEnumConverter;
-import codes.thischwa.dyndrest.model.UpdateLog;
+import java.util.Arrays;
+import java.util.List;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.transaction.TransactionManager;
 
 @Configuration
 class DatabaseTestConfig extends AbstractJdbcConfiguration {
@@ -30,9 +32,19 @@ class DatabaseTestConfig extends AbstractJdbcConfiguration {
         .build();
   }
 
+  @Bean
+  NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
+    return new NamedParameterJdbcTemplate(dataSource);
+  }
+
+  @Bean
+  TransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
   @Override
   protected List<?> userConverters() {
     return Arrays.asList(
-        new EnumToStringConverter(), new StringToEnumConverter(UpdateLog.Status.class));
+            new EnumToStringConverter<Enum<?>>(), new StringToEnumConverter<>(UpdateLog.Status.class));
   }
 }
--- src/test/java/codes/thischwa/dyndrest/config/AppConfigTest.java
+++ src/test/java/codes/thischwa/dyndrest/config/AppConfigTest.java
@@ -34,7 +34,7 @@ class AppConfigTest extends AbstractIntegrationTest {
     AppConfig.Database db = appConfig.database();
     assertNotNull(db);
     assertEquals("org.h2.Driver", db.driverClassName());
-    assertEquals("jdbc:h2:file:", db.jdbcUrlPrefix());
+    assertEquals("jdbc:h2:mem:", db.jdbcUrlPrefix());
     assertEquals("./test-db", db.file());
     assertEquals("dba", db.user());
     assertEquals("", db.password());
