dc5c822b512e3ada68eba6c8f384afb84fae431f
Thilo Schwarz <osp@thischwa.codes>
2024-06-29 20:13:14 +0200

    clean up

--- src/test/java/codes/thischwa/dyndrest/DatabaseTestConfig.java
+++ src/test/java/codes/thischwa/dyndrest/DatabaseTestConfig.java
@@ -2,20 +2,11 @@ package codes.thischwa.dyndrest;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
-import codes.thischwa.dyndrest.model.UpdateLog;
-import codes.thischwa.dyndrest.model.converter.EnumToStringConverter;
-import codes.thischwa.dyndrest.model.converter.StringToEnumConverter;
-import java.util.Arrays;
-import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.transaction.TransactionManager;
 
 @Configuration
 class DatabaseTestConfig extends AbstractJdbcConfiguration {
@@ -28,23 +19,7 @@ class DatabaseTestConfig extends AbstractJdbcConfiguration {
         .setScriptEncoding("UTF-8")
         .ignoreFailedDrops(true)
         .continueOnError(true)
-        .addScript("/h2/dump.sql")
+        .addScripts("/h2/schema.sql", "/h2/data.sql")
         .build();
-  }
-
-  @Bean
-  NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
-    return new NamedParameterJdbcTemplate(dataSource);
-  }
-
-  @Bean
-  TransactionManager transactionManager(DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
-  }
-
-  @Override
-  protected List<?> userConverters() {
-    return Arrays.asList(
-            new EnumToStringConverter<Enum<?>>(), new StringToEnumConverter<>(UpdateLog.Status.class));
   }
 }
