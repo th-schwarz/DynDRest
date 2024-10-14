db44856a754bf3062848b6a309438004c6bab519
Thilo Schwarz <osp@thischwa.codes>
2024-10-14 19:08:01 +0200

    remove obsolete objects

--- src/main/java/codes/thischwa/dyndrest/model/config/ZoneConfig.java
+++
@@ -1,9 +0,0 @@
-package codes.thischwa.dyndrest.model.config;
-
-import codes.thischwa.dyndrest.model.Zone;
-import java.util.List;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-/** The Configuration of a zone. */
-@ConfigurationProperties
-public record ZoneConfig(List<Zone> zones) {}
