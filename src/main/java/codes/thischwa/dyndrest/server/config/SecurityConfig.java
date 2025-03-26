35a05a217a3efe0df395e918fc2c070a086bcf57
Thilo Schwarz <osp@thischwa.codes>
2025-03-26 18:41:47 +0100

    Refactor SecurityConfig to improve constant naming.
    Renamed variables and constants to follow consistent naming conventions, improving clarity and code readability. Updated references across the class to reflect these changes without modifying functionality.

--- src/main/java/codes/thischwa/dyndrest/server/config/SecurityConfig.java
+++ src/main/java/codes/thischwa/dyndrest/server/config/SecurityConfig.java
@@ -42,14 +42,14 @@ public class SecurityConfig {
   static final String ROLE_LOGVIEWER = "LOGVIEWER";
   static final String ROLE_USER = "USER";
   static final String ROLE_HEALTH = "HEALTH";
-  private static final List<String> publicPaths =
+  private static final List<String> PUBLIC_ENDPOINTS =
       new ArrayList<>(List.of("/", "/favicon.ico", "/error"));
   public final Environment env;
   private final AppConfig appConfig;
-  private final PasswordEncoder encoder =
+  private static final PasswordEncoder PASSWORD_ENCODER =
       PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  private static final String[] loguiPaths = {"/log-ui", "/log-ui/*"};
-  private static final String adminPath = "/admin/**";
+  private static final String[] LOG_UI_ENDPOINTS = {"/log-ui", "/log-ui/*"};
+  private static final String ADMIN_ENDPOINT = "/admin/**";
 
   private final boolean updateLogEnabled;
 
@@ -94,9 +94,9 @@ public class SecurityConfig {
             && StringUtils.hasText(appConfig.adminApiToken());
 
     if (Arrays.asList(env.getActiveProfiles()).contains("opendoc")) {
-      publicPaths.add("/v3/api-docs*");
+      PUBLIC_ENDPOINTS.add("/v3/api-docs*");
     }
-    log.info("Public paths: {}", String.join(",", publicPaths));
+    log.info("Public paths: {}", String.join(",", PUBLIC_ENDPOINTS));
   }
 
   /**
@@ -135,7 +135,7 @@ public class SecurityConfig {
     }
     udm.createUser(
         User.builder()
-            .passwordEncoder(encoder::encode)
+            .passwordEncoder(PASSWORD_ENCODER::encode)
             .username(userName)
             .password(password)
             .roles(role)
@@ -165,7 +165,7 @@ public class SecurityConfig {
     if (updateLogEnabled) {
       // enable security for the log-view
       http.authorizeHttpRequests(
-          req -> req.requestMatchers(buildMatchers(loguiPaths)).hasAnyRole(ROLE_LOGVIEWER));
+          req -> req.requestMatchers(buildMatchers(LOG_UI_ENDPOINTS)).hasAnyRole(ROLE_LOGVIEWER));
     }
 
     if (healthEnabled) {
@@ -179,7 +179,7 @@ public class SecurityConfig {
     if (adminEnabled) {
       // enables security for the admin paths
       http.authorizeHttpRequests(
-              req -> req.requestMatchers(buildMatchers(adminPath)).hasRole(ROLE_ADMIN))
+              req -> req.requestMatchers(buildMatchers(ADMIN_ENDPOINT)).hasRole(ROLE_ADMIN))
           .sessionManagement(
               session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .csrf(AbstractHttpConfigurer::disable);
@@ -187,7 +187,7 @@ public class SecurityConfig {
 
     // public routes
     http.authorizeHttpRequests(
-        req -> req.requestMatchers(buildMatchers(publicPaths.toArray(new String[0]))).permitAll());
+        req -> req.requestMatchers(buildMatchers(PUBLIC_ENDPOINTS.toArray(new String[0]))).permitAll());
 
     // enable basic-auth and ROLE_USER for all other routes
     // it's a rest-api, so there is no need for session handling and csrf
