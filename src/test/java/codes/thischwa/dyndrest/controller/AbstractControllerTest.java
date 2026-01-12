e74e80d3809151ff7d836fd5378cac59aa9e6c63
Thilo Schwarz <osp@thischwa.codes>
2026-01-12 15:22:36 +0100

    test: activate test profile in `AbstractControllerTest` through `@ActiveProfiles` annotation

--- src/test/java/codes/thischwa/dyndrest/controller/AbstractControllerTest.java
+++ src/test/java/codes/thischwa/dyndrest/controller/AbstractControllerTest.java
@@ -14,9 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 abstract class AbstractControllerTest {
 
   @TestConfiguration
