bc9af1225ef8b99c21706707d62f1b31cc5f2ce5
Thilo Schwarz <osp@thischwa.codes>
2026-01-18 13:38:42 +0100

    docs: downgrade OpenAPI spec version in `ApiRoutes` to 0.3 for consistency

--- src/main/java/codes/thischwa/dyndrest/server/controller/ApiRoutes.java
+++ src/main/java/codes/thischwa/dyndrest/server/controller/ApiRoutes.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RequestParam;
         @Info(
             title = "A Dynamic DNS REST Service.",
             description = "The routes of the dynamic DNS API",
-            version = "0.4",
+            version = "0.3",
             contact =
                 @Contact(
                     name = "Thilo Schwarz",
